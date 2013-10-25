b0574d5064d6981a90d87e6bb614bb0dff9a7ad5
Eric Bréchemier <eric@bezen.org>
2013-10-25 12:52:47 +0200

    Add scripts to convert an IP to a country code or a country name

---
+++ ip2country-code.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+# Convert an IPv4 address to a 2-letter country code
+#
+# Usage: ip2country-code.pl ipAddress
+# where ipAddress is an IPv4 address (###.###.###.###)
+my $ipAddress = $ARGV[0];
+# Config
+my $geoipfreeDataPath='data/IpToCountry.dat';
+
+if ( $ipAddress eq '' ) {
+  print "Usage: ip2country-code.pl ipAddress\n";
+  exit 1;
+}
+
+use File::Basename;
+use Geo::IPfree;
+
+chdir dirname($0);
+
+my $geoIpFree = Geo::IPfree->new( $geoipfreeDataPath );
+my ( $countryCode, $countryName ) = $geoIpFree->LookUp( $ipAddress );
+
+print $countryCode."\n";
